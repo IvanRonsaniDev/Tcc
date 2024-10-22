4d8d1b565a682a6812b0556956699c2ab668cdc3
Lucas Moro <lucas.rmoro1@gmail.com>
2024-10-22 01:25:31 -0300

    Fixing navigation

--- app/src/main/java/com/example/tcc/HomeActivity.kt
+++ app/src/main/java/com/example/tcc/HomeActivity.kt
@@ -6,30 +6,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.tcc.databinding.ActivityHomeBinding
 
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var navController: NavController
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-
-        initNavigation ()
-
+        initNavigation()
     }
 
-    private fun initNavigation (){
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmento_principal) as NavHostFragment
-        navController = navHostFragment.navController
-        NavigationUI.setupWithNavController( binding.bnNavigation, navController)
-
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.fragmentoPrincipal.id) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnNavigation.setupWithNavController(navController)
     }
 }
