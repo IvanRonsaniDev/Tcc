1149c1b739c8904f3906468b54b5d26fee04d1dd
Navixdev <ivanronsani100@gmail.com>
2024-10-21 23:53:13 -0300

    fazendo um commit

--- app/src/main/java/com/example/tcc/home.kt
+++ app/src/main/java/com/example/tcc/home.kt
@@ -6,20 +6,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.tcc.databinding.ActivityHomeBinding
 
 
 class home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
 
+        initNavigation ()
 
+    }
 
-
-
-
+    private fun initNavigation (){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmento_principal) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController( binding.bnNavigation, navController)
 
     }
 }
